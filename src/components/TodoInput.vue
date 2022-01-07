695cfd432c1ff200c363c129959a8053d0e02ae5
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2022-01-07 11:30:22 +0900

    :zap: Update Methods

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -13,8 +13,11 @@ export default {
   methods: {
     addTodo() {
       // 저장하는 로직
-      localStorage.setItem(this.newTodoItem, this.newTodoItem);
-      this.clearInput();
+      if (this.newTodoItem !== "") {
+        var obj = { completed: false, item: this.newTodoItem };
+        localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+        this.clearInput();
+      }
     },
     clearInput() {
       this.newTodoItem = "";
